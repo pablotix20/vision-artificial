460b02b2f4bfb4407e3d6799257d5f5feeed36fe
Pablo <pablocriadoalbillos@gmail.com>
2021-02-28 11:55:29 +0100

    Finish P3

--- P2/P3.py
+++ P2/P3.py
@@ -13,42 +13,45 @@ def draw_circle(event, x, y, flags, param):
         cv2.circle(img, (x, y), 5, (255, 0, 255), 0)
         points.append([x, y])
 
-# Create a black image, a window and bind the function to window
 
-
-#img = cv2.imread('../imagenes/caja_persp.png')
-img = cv2.imread('./images/casa1.jpg')
+img = cv2.imread('./images/induva.jpg')
 img_size = (img.shape[1], img.shape[0])
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.setMouseCallback('image', draw_circle)
-while(len(points) < 4):
+while len(points) < 4:
     cv2.imshow('image', img)
     cv2.waitKey(10)
+cv2.destroyAllWindows()
 
 points_source = np.float32(points)
-
 points_destination_cropped = np.float32(
     [[0, 0], [img_size[0], 0], [img_size[0], img_size[1]], [0, img_size[1]]])
 points_destination_full = np.float32(
-    [[img_size[0]*.2, img_size[1]*.2], [img_size[0]*.8, img_size[1]*.2], [img_size[0]*.8, img_size[1]*.8], [img_size[0]*.2, img_size[1]*.8]])
-cv2.destroyAllWindows()
+    [[img_size[0]*.25, img_size[1]*.25], [img_size[0]*.75, img_size[1]*.25], [img_size[0]*.75, img_size[1]*.75], [img_size[0]*.25, img_size[1]*.75]])
 
 transform_matrix_crop = cv2.getPerspectiveTransform(
     points_source, points_destination_cropped)
-transformed_image_cropped = cv2.warpPerspective(
-    img, transform_matrix_crop, img_size)
+transformed_image_cropped = cv2.cvtColor(cv2.warpPerspective(
+    img, transform_matrix_crop, img_size), cv2.COLOR_BGR2RGB)
 transform_matrix_full = cv2.getPerspectiveTransform(
     points_source, points_destination_full)
-transformed_image_full = cv2.warpPerspective(
-    img, transform_matrix_full, img_size)
+transformed_image_full = cv2.cvtColor(cv2.warpPerspective(
+    img, transform_matrix_full, img_size), cv2.COLOR_BGR2RGB)
+
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 plt.subplot(2, 2, 1), plt.title('Original'), plt.imshow(
-    img), plt.axis(False)
-plt.subplot(2, 2, 2), plt.title('Editada 1'), plt.imshow(
+    img_rgb), plt.axis(False)
+plt.subplot(2, 2, 2), plt.title('Recorte'), plt.imshow(
     transformed_image_cropped), plt.axis(False)
-plt.subplot(2, 2, 3), plt.title('Editada 2'), plt.imshow(
+plt.subplot(2, 2, 3), plt.title('Vista global'), plt.imshow(
     transformed_image_full), plt.axis(False)
-# plt.subplot(2, 2, 4), plt.title('Editada 3'), plt.imshow(
-#     img3, 'gray', vmin=0, vmax=255), plt.axis(False)
+plt.show()
+
+plt.subplot(1, 1, 1), plt.title('Recorte'), plt.imshow(
+    transformed_image_cropped), plt.axis(False)
+plt.show()
+plt.subplot(1, 1, 1), plt.title('Vista global'), plt.imshow(
+    transformed_image_full), plt.axis(False)
 plt.show()
